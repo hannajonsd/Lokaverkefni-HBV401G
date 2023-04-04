63466e066d20b93f72663a446b46446fa0e1e4d1
hannajonsd <hanna.m.jonsd@gmail.com>
2023-04-04 14:44:41 +0000

    repositories

---
+++ src/main/java/repositories/BookingRepository.java
@@ -0,0 +1,21 @@
+package repositories;
+
+public class BookingRepository implements Repository {
+
+    private DatabaseInstance db;
+
+
+    public void getUser(){
+
+    }
+
+    public void newUser(){
+
+    }
+
+    public void bookRoom(){
+
+    }
+
+
+}
---
+++ src/main/java/repositories/HotelRepository.java
@@ -0,0 +1,22 @@
+package repositories;
+import sqlite3;
+
+public class HotelRepository implements Repository{
+
+    private DatabaseInstance db;
+
+    public void hotels(){
+    }
+
+    public void hotelView(){
+    }
+
+    public void available(){
+
+    }
+
+    public String getReview(){
+
+    }
+
+}

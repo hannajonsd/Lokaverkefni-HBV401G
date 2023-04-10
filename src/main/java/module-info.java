0887e948ffcdce149020fc090f8cc6da1af7b1da
benso02 <bet14@hi.is>
2023-04-10 17:37:55 +0000

    innskráning komin sorry hanna

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.lokaverkefnihbv401g {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.xerial.sqlitejdbc;
 
 
     opens vidmot to javafx.fxml;
--- src/main/java/vidmot/HotelController.java
+++ src/main/java/vidmot/HotelController.java
@@ -1,19 +1,28 @@
 package vidmot;
 
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
 import vinnsla.GetDatabaseConn;
 import vinnsla.Hotel;
 import vinnsla.User;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 
 public class HotelController {
@@ -24,12 +33,12 @@ public class HotelController {
     public Button fxVelja;
     public User user;
     public Button fxRegister;
+    public Button fxLogin;
 
     public void initialize(){
-        UserDialog ud = new UserDialog();
+        UserDialog ud=new UserDialog();
         user= ud.getUser();
         fxVelja.disableProperty().bind(Bindings.isEmpty(hotels.getSelectionModel().getSelectedItems()));
-
     }
 
 
@@ -68,9 +77,113 @@ public class HotelController {
         ViewSwitcher.switchTo(View.HOTEL);
     }
 
-    public void register(ActionEvent actionEvent) {
-        UserDialog ud = new UserDialog();
+    public void register() {
+        UserDialog ud= new UserDialog();
         user= ud.getUser();
+    }
+
+    public void login() {
+        textDialog();
+    }
+
+    public void textDialog() {
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Innskráning");
+        dialog.setHeaderText("Skráðu inn nafn og lykilorð");
+
+        // Set the button types
+        ButtonType loginButtonType = new ButtonType("Innskrá", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        // Create the GridPane for the custom content
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new javafx.geometry.Insets(20));
+
+        // Create two text fields
+        TextField usernameField = new TextField();
+        PasswordField passwordField = new PasswordField();
+        grid.add(new Label("Nafn:"), 0, 0);
+        grid.add(usernameField, 1, 0);
+        grid.add(new Label("Lykilorð:"), 0, 1);
+        grid.add(passwordField, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+
+        Platform.runLater(usernameField::requestFocus);
+
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(usernameField.getText(), passwordField.getText());
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+
+        result.ifPresent(values -> {
+            String username = values.getKey();
+            String password = values.getValue();
+            if (!isUsernameIndb(username)) {
+                dialog.setHeaderText("Notendanafn ekki til");
+            } else if(!doPasswordMatch(password, username)){
+                dialog.setHeaderText("Vitlaust lykilorð");
+            }else{
+                user= getUserbyName(username);
+                System.out.println(user);
+            }
+        });
+    }
+    public boolean isUsernameIndb(String username) {
+        GetDatabaseConn dbconn = new GetDatabaseConn();
+        Connection conn = dbconn.getDBconnection();
+        String query = "SELECT * FROM user WHERE name=?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean doPasswordMatch(String password, String username) {
+        GetDatabaseConn dbconn = new GetDatabaseConn();
+        Connection conn = dbconn.getDBconnection();
+        String query = "SELECT * FROM user WHERE name=?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            String passwordDB = rs.getString("password");
+            return password.equals(passwordDB);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User getUserbyName(String username) {
+        GetDatabaseConn dbconn = new GetDatabaseConn();
+        Connection conn = dbconn.getDBconnection();
+        String query = "SELECT * FROM user WHERE name=?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            StringProperty name = new SimpleStringProperty(username);
+            StringProperty password = new SimpleStringProperty(rs.getString("password"));
+            StringProperty email = new SimpleStringProperty(rs.getString("email"));
+            StringProperty ssn = new SimpleStringProperty(rs.getString("ssn"));
+            return new User(name, email, ssn, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
 

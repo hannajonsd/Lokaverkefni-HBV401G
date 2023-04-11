c8093911afa11b83ac20ff5c3a958c02865a7168
benso02 <bet14@hi.is>
2023-04-11 13:17:47 +0000

    meira login og register update

--- src/main/java/vidmot/HotelController.java
+++ src/main/java/vidmot/HotelController.java
@@ -30,14 +30,18 @@ public class HotelController {
     public HotelSidaView hotels;
     private static ObjectProperty<Hotel> hotel = new SimpleObjectProperty<>();
     public HotelView hotelView;
+    @FXML
     public Button fxVelja;
     public User user;
+    @FXML
     public Button fxRegister;
+    @FXML
     public Button fxLogin;
+    @FXML
+    public Label fxUser;
 
     public void initialize() {
-        UserDialog ud = new UserDialog();
-        user = ud.getUser();
+        textDialog();
         fxVelja.disableProperty().bind(Bindings.isEmpty(hotels.getSelectionModel().getSelectedItems()));
     }
 
@@ -78,120 +82,18 @@ public class HotelController {
         ViewSwitcher.switchTo(View.HOTEL);
     }
 
-    public void register() {
-        UserDialog ud = new UserDialog();
-        user = ud.getUser();
-    }
-
     public void login() {
         textDialog();
     }
+    public void updateUserName() {
+        fxUser.setText(user.getNafn());
+    }
 
     public void textDialog() {
-        Dialog<Pair<String, String>> dialog = new Dialog<>();
-        dialog.setTitle("Innskráning");
-        dialog.setHeaderText("Skráðu inn nafn og lykilorð");
-
-        ButtonType loginButtonType = new ButtonType("Innskrá", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
-
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new javafx.geometry.Insets(20));
-
-        TextField usernameField = new TextField();
-        PasswordField passwordField = new PasswordField();
-        grid.add(new Label("Nafn:"), 0, 0);
-        grid.add(usernameField, 1, 0);
-        grid.add(new Label("Lykilorð:"), 0, 1);
-        grid.add(passwordField, 1, 1);
-
-        dialog.getDialogPane().setContent(grid);
-
-        Platform.runLater(usernameField::requestFocus);
-
-
-        dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == loginButtonType) {
-                return new Pair<>(usernameField.getText(), passwordField.getText());
-            }
-            return null;
-        });
-
-
-        AtomicBoolean closeDialog = new AtomicBoolean(false); // Flag to determine if dialog should be closed
-
-        while (!closeDialog.get()) { // Loop to keep the dialog open if needed
-            Optional<Pair<String, String>> result = dialog.showAndWait();
-
-            result.ifPresent(values -> {
-                String username = values.getKey();
-                String password = values.getValue();
-                if (!isUsernameInDb(username)) {
-                    dialog.setHeaderText("Notendanafn ekki til");
-                } else if (!doPasswordMatch(password, username)) {
-                    dialog.setHeaderText("Vitlaust lykilorð");
-                } else {
-                    user = getUserByName(username);
-                    System.out.println(user);
-                    closeDialog.set(true); // Set flag to close dialog
-                }
-            });
-
-            // If result is not present, dialog was cancelled, set flag to close dialog
-            if (result.isEmpty()) {
-                closeDialog.set(true);
-            }
-        }
-    }
-    public boolean isUsernameInDb(String username) {
-        GetDatabaseConn dbconn = new GetDatabaseConn();
-        Connection conn = dbconn.getDBconnection();
-        String query = "SELECT * FROM user WHERE name=?";
-        try{
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, username);
-            ResultSet rs = stmt.executeQuery();
-            return rs.next();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public boolean doPasswordMatch(String password, String username) {
-        GetDatabaseConn dbconn = new GetDatabaseConn();
-        Connection conn = dbconn.getDBconnection();
-        String query = "SELECT * FROM user WHERE name=?";
-        try{
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, username);
-            ResultSet rs = stmt.executeQuery();
-            String passwordDB = rs.getString("password");
-            return password.equals(passwordDB);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public User getUserByName(String username) {
-        GetDatabaseConn dbconn = new GetDatabaseConn();
-        Connection conn = dbconn.getDBconnection();
-        String query = "SELECT * FROM user WHERE name=?";
-        try{
-            PreparedStatement stmt = conn.prepareStatement(query);
-            stmt.setString(1, username);
-            ResultSet rs = stmt.executeQuery();
-            StringProperty name = new SimpleStringProperty(username);
-            StringProperty password = new SimpleStringProperty(rs.getString("password"));
-            StringProperty email = new SimpleStringProperty(rs.getString("email"));
-            StringProperty ssn = new SimpleStringProperty(rs.getString("ssn"));
-            return new User(name, email, ssn, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+        LoginDialog ld = new LoginDialog();
+        user = ld.getUser();
+        if(user!=null){
+            updateUserName();
         }
     }
 }
---
+++ src/main/java/vidmot/LoginDialog.java
@@ -0,0 +1,121 @@
+package vidmot;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.util.Pair;
+import vinnsla.GetDatabaseConn;
+import vinnsla.User;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class LoginDialog extends Dialog<User>{
+    public Button fxNyskraning;
+    @FXML
+    private DialogPane dialogPane;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    public LoginDialog() {
+        setDialogPane(readUser());
+        fxNyskraning.setOnAction(event -> {
+          register();
+        });
+        setResultConverter(b -> {
+            if(b.getButtonData()==ButtonBar.ButtonData.OK_DONE) {
+                String username = usernameField.getText();
+                String password = passwordField.getText();
+                if (!isUsernameInDb(username)) {
+                    setHeaderText("Nafn ekki til");
+                } else if (doPasswordMatch(username, password)) {
+                    setHeaderText("Vitlaust lykilorð");
+
+                } else {
+                    User user = getUserByName(username);
+                    System.out.println(user);
+                   return user;
+                }
+            }
+            return null;
+        });
+    }
+    public User getUser(){
+        Optional<User> u = showAndWait();
+        return u.orElse(null);
+    }
+
+    private DialogPane readUser(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        try {
+            fxmlLoader.setController(this);
+            return fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    public boolean isUsernameInDb(String username) {
+        GetDatabaseConn dbconn = new GetDatabaseConn();
+        Connection conn = dbconn.getDBconnection();
+        String query = "SELECT * FROM user WHERE name=?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean doPasswordMatch(String password, String username) {
+        GetDatabaseConn dbconn = new GetDatabaseConn();
+        Connection conn = dbconn.getDBconnection();
+        String query = "SELECT * FROM user WHERE name=?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            String passwordDB = rs.getString("password");
+            return password.equals(passwordDB);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User getUserByName(String username) {
+        GetDatabaseConn dbconn = new GetDatabaseConn();
+        Connection conn = dbconn.getDBconnection();
+        String query = "SELECT * FROM user WHERE name=?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            StringProperty name = new SimpleStringProperty(username);
+            StringProperty password = new SimpleStringProperty(rs.getString("password"));
+            StringProperty email = new SimpleStringProperty(rs.getString("email"));
+            StringProperty ssn = new SimpleStringProperty(rs.getString("ssn"));
+            return new User(name, email, ssn, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void register() {
+        UserDialog ud = new UserDialog();
+        User user = ud.getUser();
+    }
+}
